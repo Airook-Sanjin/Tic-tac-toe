8c851aadad15004d91ef9476fc897262049e1e95
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2024-10-24 11:06:06 -0400

    Tie works now

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let turn = 1;
 let p1Score = 0;
 let p2score = 0;
-let a1, a2, a3, c1, c2, c3, b1, b2, b3; 
+let a1, a2, a3, c1, c2, c3, b1, b2, b3;
 a1 = document.getElementById("a1");
 a2 = document.getElementById("a2");
 a3 = document.getElementById("a3");
@@ -12,155 +12,151 @@ b1 = document.getElementById("b1");
 b2 = document.getElementById("b2");
 b3 = document.getElementById("b3");
 let score = document.getElementById("p1score");
-let player2Score=document.getElementById("p2score")
+let player2Score = document.getElementById("p2score");
 let tie;
 
 // Function to indicate player turn
 function playerTurn(inputBox) {
-    if (inputBox.value === "") {
-        if (turn === 1) {
-            inputBox.value = `✖`;
-            console.log("X");
-            turn = 0;
-        } else {
-            inputBox.value = `◯`;
-            console.log("0");
-            turn = 1;
-        }
+  if (inputBox.value === "") {
+    if (turn === 1) {
+      inputBox.value = `✖`;
+      console.log("X");
+      turn = 0;
+    } else {
+      inputBox.value = `◯`;
+      console.log("0");
+      turn = 1;
     }
+  }
 }
 
-function whoWonX(){
-    if(a1.value ==="✖" && a2.value==="✖" && a3.value==="✖"){ 
-        p1Score++
-    score.innerText = `Score: ${p1Score}`
-    console.log(score.innerText)
-    tie = false;
-    }
-    else if(b1.value ==="✖" && b2.value==="✖" && b3.value==="✖"){ 
-        p1Score++
-    score.innerText = `Score: ${p1Score}`
-    console.log(score.innerText)
-    tie = false;
-    }
-    else if(c1.value ==="✖" && c2.value==="✖" && c3.value==="✖"){ 
-        p1Score++
-    score.innerText = `Score: ${p1Score}`
-    console.log(score.innerText)
-    tie = false;
-    }
-    else if(a1.value ==="✖" && b1.value==="✖" && c1.value==="✖"){ 
-        p1Score++
-    score.innerText = `Score: ${p1Score}`
-    console.log(score.innerText)
-    tie = false;
-    }
-    else if(a2.value ==="✖" && b2.value==="✖" && c2.value==="✖"){ 
-        p1Score++
-    score.innerText = `Score: ${p1Score}`
-    console.log(score.innerText)
-    tie = false;
-    }
-    else if(a3.value ==="✖" && b3.value==="✖" && c3.value==="✖"){ 
-        p1Score++
-    score.innerText = `Score: ${p1Score}`
-    console.log(score.innerText)
-    tie = false;
-    }
-    else if(a1.value ==="✖" && b2.value==="✖" && c3.value==="✖"){ 
-        p1Score++
-    score.innerText = `Score: ${p1Score}`
-    console.log(score.innerText)
-    tie = false;
-    }
-    else if(c1.value ==="✖" && b2.value==="✖" && a3.value==="✖"){ 
-        p1Score++
-    score.innerText = `Score: ${p1Score}`
-    console.log(score.innerText)
-    tie = false;
-    } else {
-        return isTie();
-    }
+function whoWonX() {
+  if (a1.value === "✖" && a2.value === "✖" && a3.value === "✖") {
+    p1Score++;
+    score.innerText = `Score: ${p1Score}`;
+    console.log(score.innerText);
+    gameReset();
+  } else if (b1.value === "✖" && b2.value === "✖" && b3.value === "✖") {
+    p1Score++;
+    score.innerText = `Score: ${p1Score}`;
+    console.log(score.innerText);
+    gameReset();
+  } else if (c1.value === "✖" && c2.value === "✖" && c3.value === "✖") {
+    p1Score++;
+    score.innerText = `Score: ${p1Score}`;
+    console.log(score.innerText);
+    gameReset();
+  } else if (a1.value === "✖" && b1.value === "✖" && c1.value === "✖") {
+    p1Score++;
+    score.innerText = `Score: ${p1Score}`;
+    console.log(score.innerText);
+    gameReset();
+  } else if (a2.value === "✖" && b2.value === "✖" && c2.value === "✖") {
+    p1Score++;
+    score.innerText = `Score: ${p1Score}`;
+    console.log(score.innerText);
+    gameReset();
+  } else if (a3.value === "✖" && b3.value === "✖" && c3.value === "✖") {
+    p1Score++;
+    score.innerText = `Score: ${p1Score}`;
+    console.log(score.innerText);
+    gameReset();
+  } else if (a1.value === "✖" && b2.value === "✖" && c3.value === "✖") {
+    p1Score++;
+    score.innerText = `Score: ${p1Score}`;
+    console.log(score.innerText);
+    gameReset();
+  } else if (c1.value === "✖" && b2.value === "✖" && a3.value === "✖") {
+    p1Score++;
+    score.innerText = `Score: ${p1Score}`;
+    console.log(score.innerText);
+    gameReset();
+  } else {
+    return isTie();
+  }
 }
-function calculatePoints(){
-whoWonX();
-whoWonO();
-isTie();
+function calculatePoints() {
+  whoWonX();
+  whoWonO();
+  isTie();
 }
 
 // Functions to indicate winner
 function whoWonO() {
-    let player2Score = document.getElementById("p2score");
-    if (a1.value == "◯" && a2.value == "◯" && a3.value == "◯") {
-        console.log("Player 2 Won!");
-        p2score++;
-        player2Score.textContent = `Score: ${p2score}`;
-        tie = false;
-    } 
-    else if (b1.value == "◯" && b2.value == "◯" && b3.value == "◯") {
-        console.log("Player 2 Won!");
-        p2score++;
-        player2Score.textContent = `Score: ${p2score}`;
-        tie = false;
-    }
-    else if (c1.value == "◯" && c2.value == "◯" && c3.value == "◯") {
-        console.log("Player 2 Won!");
-        p2score++;
-        player2Score.textContent = `Score: ${p2score}`;
-        tie = false;
-    }
-    else if (a1.value == "◯" && b1.value == "◯" && c1.value == "◯") {
-        console.log("Player 2 Won!");
-        p2score++;
-        player2Score.textContent = `Score: ${p2score}`;
-        tie = false;
-    }
-    else if (a2.value == "◯" && b2.value == "◯" && c2.value == "◯") {
-        console.log("Player 2 Won!");
-        p2score++;
-        player2Score.textContent = `Score: ${p2score}`;
-        tie = false;
-    }
-    else if (a3.value == "◯" && b3.value == "◯" && c3.value == "◯") {
-        console.log("Player 2 Won!");
-        p2score++;
-        player2Score.textContent = `Score: ${p2score}`;
-        tie = false;
-    }
-    else if (a1.value == "◯" && b2.value == "◯" && c3.value == "◯") {
-        console.log("Player 2 Won!");
-        p2score++;
-        player2Score.textContent = `Score: ${p2score}`;
-        tie = false;
-    }
-    else if (a3.value == "◯" && b2.value == "◯" && c1.value == "◯") {
-        console.log("Player 2 Won!");
-        p2score++;
-        player2Score.textContent = `Score: ${p2score}`;
-        tie = false;
-    } else {
-        return tie;
-    }
+  let player2Score = document.getElementById("p2score");
+  if (a1.value == "◯" && a2.value == "◯" && a3.value == "◯") {
+    console.log("Player 2 Won!");
+    p2score++;
+    player2Score.textContent = `Score: ${p2score}`;
+    gameReset();
+  } else if (b1.value == "◯" && b2.value == "◯" && b3.value == "◯") {
+    console.log("Player 2 Won!");
+    p2score++;
+    player2Score.textContent = `Score: ${p2score}`;
+    gameReset();
+  } else if (c1.value == "◯" && c2.value == "◯" && c3.value == "◯") {
+    console.log("Player 2 Won!");
+    p2score++;
+    player2Score.textContent = `Score: ${p2score}`;
+    gameReset();
+  } else if (a1.value == "◯" && b1.value == "◯" && c1.value == "◯") {
+    console.log("Player 2 Won!");
+    p2score++;
+    player2Score.textContent = `Score: ${p2score}`;
+    gameReset();
+  } else if (a2.value == "◯" && b2.value == "◯" && c2.value == "◯") {
+    console.log("Player 2 Won!");
+    p2score++;
+    player2Score.textContent = `Score: ${p2score}`;
+    gameReset();
+  } else if (a3.value == "◯" && b3.value == "◯" && c3.value == "◯") {
+    console.log("Player 2 Won!");
+    p2score++;
+    player2Score.textContent = `Score: ${p2score}`;
+    gameReset();
+  } else if (a1.value == "◯" && b2.value == "◯" && c3.value == "◯") {
+    console.log("Player 2 Won!");
+    p2score++;
+    player2Score.textContent = `Score: ${p2score}`;
+    gameReset();
+  } else if (a3.value == "◯" && b2.value == "◯" && c1.value == "◯") {
+    console.log("Player 2 Won!");
+    p2score++;
+    player2Score.textContent = `Score: ${p2score}`;
+    gameReset();
+  } else {
+    return tie;
+  }
 }
 
 function isTie() {
+  if (
+    (a1.value === "✖" || a1.value === "◯") &&
+    (a2.value === "✖" || a2.value === "◯") &&
+    (a3.value === "✖" || a3.value === "◯") &&
+    (b1.value === "✖" || b1.value === "◯") &&
+    (b2.value === "✖" || b2.value === "◯") &&
+    (b3.value === "✖" || b3.value === "◯") &&
+    (c1.value === "✖" || c2.value === "◯") &&
+    (c2.value === "✖" || c2.value === "◯") &&
+    (c3.value === "✖" || c3.value === "◯")
+  ) {
     let tiescore = 0;
     let tiep = document.getElementById("tiescore");
-    if (tie = true) {
-        tiescore++;
-        tiep.textContent = `Tie: ${tiescore}`;
-        gameReset();
-    }
+    tiescore++;
+    tiep.textContent = `Tie: ${tiescore}`;
+    gameReset();
+  }
 }
 
 // Function to reset gameboard
 resetbtn = document.getElementById("rst-btn");
-resetbtn.addEventListener("click", gameReset)
+resetbtn.addEventListener("click", gameReset);
 
 function gameReset() {
-    let allInputs = document.querySelectorAll('input[type="button"]')
-    allInputs.forEach(input => {
-        input.value = "";
-    });
-    turn = 1;
+  let allInputs = document.querySelectorAll('input[type="button"]');
+  allInputs.forEach((input) => {
+    input.value = "";
+  });
+  turn = 1;
 }
