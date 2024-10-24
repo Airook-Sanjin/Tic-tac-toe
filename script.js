11d4dd8411c81f0fc0f954df891c989161ecb9d8
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2024-10-24 09:20:33 -0400

    changed the styles some more and added functionality to the resetboardbtn

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 let turn = 1;
 
+
 // Functions to indicate player turn
 function playerTurn(inputBox) {
-    if (inputBox == " ") {
+    if (inputBox.value === "") {
         if (turn == 1) {
-            inputBox.innerHTML = <ion-icon name="close-outline"></ion-icon>;
+            inputBox.value = `X`;
             console.log("X");
             turn = 0;
         } else {
-            inputBox.innerHTML = <ion-icon name="ellipse-outline"></ion-icon>;
+            inputBox.value = `O`;
             console.log("0");
             turn = 1;
         }
     }
     
 }
-
+///
+resetbtn = document.getElementById("rst-btn");
+resetbtn.addEventListener("click", gameReset)
 
 function gameReset() {
     let allInputs = document.querySelectorAll('input[type="button"]')
-    allInputs.value = "";
+    allInputs.forEach(input => {
+        input.value = "";
+    });
     turn = 1;
 }
