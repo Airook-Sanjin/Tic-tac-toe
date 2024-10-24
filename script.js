44a315784b36092b7e7a1da5debe92d45f68ece9
Tyler M. <tjmcdowell726@gmail.com>
2024-10-24 10:30:23 -0400

    random

--- script.js
+++ script.js
@@ -13,6 +13,50 @@ b2 = document.getElementById("b2");
 b3 = document.getElementById("b3");
 let score = document.getElementById("p1score");
 
+
+// Functions to indicate winner
+let p2score = 0;
+function whoWonO() {
+    let player2Score = document.getElementById("p2score");
+    if (a1.value == "◯" && a2.value == "◯" && a3.value == "◯") {
+        console.log("Player 2 Won!");
+        p2score++;
+        player2Score.textContent = `Score: ${p2score}`;
+    } 
+    else if (b1.value == "◯" && b2.value == "◯" && b3.value == "◯") {
+        console.log("Player 2 Won!");
+        p2score++;
+        player2Score.textContent = `Score: ${p2score}`;
+    }
+    else if (c1.value == "◯" && c2.value == "◯" && c3.value == "◯") {
+        console.log("Player 2 Won!");
+        p2score++;
+        player2Score.textContent = `Score: ${p2score}`;
+    }
+    else if (a1.value == "◯" && b1.value == "◯" && c1.value == "◯") {
+        console.log("Player 2 Won!");
+        p2score++;
+        player2Score.textContent = `Score: ${p2score}`;
+    }
+    else if (a2.value == "◯" && b2.value == "◯" && c2.value == "◯") {
+        console.log("Player 2 Won!");
+        p2score++;
+        player2Score.textContent = `Score: ${p2score}`;
+    }
+    else if (a3.value == "◯" && b3.value == "◯" && c3.value == "◯") {
+        console.log("Player 2 Won!");
+        p2score++;2
+        player2Score.textContent = `Score: ${p2score}`;
+    }
+    else if (a1.value == "◯" && b2.value == "◯" && c3.value == "◯") {
+        console.log("Player 2 Won!");
+        p2score++;
+        player2Score.textContent = `Score: ${p2score}`;
+    }
+    else if (a3.value == "◯" && b2.value == "◯" && c1.value == "◯") {
+        console.log("Player 2 Won!");
+        p2score++;
+        player2Score.textContent = `Score: ${p2score}`;
 // Functions to indicate player turn
 function playerTurn(inputBox) {
     if (inputBox.value === "") {
